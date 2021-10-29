5fbb60336c54041a53e509582d0638e415fc9a39
LukeRel <91571985+LukeRel@users.noreply.github.com>
2021-10-29 20:31:21 +0200

    Update opensees.bat

--- TEST/Regularization/opensees.bat
+++ TEST/Regularization/opensees.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-set PATH=C:\OpenSees\Win64\bin;C:\OpenSees-Solvers\tcl\bin;C:\OpenSees-Solvers\hdf5;C:\OpenSees-Solvers\bin;C:\OpenSees-Solvers\plugins;%PATH%
+set PATH=C:\OpenSees-FPA\Win64\bin;C:\OpenSees-Solvers\tcl\bin;C:\OpenSees-Solvers\hdf5;C:\OpenSees-Solvers\bin;C:\OpenSees-Solvers\plugins;%PATH%
 OpenSees.exe %1
 pause
